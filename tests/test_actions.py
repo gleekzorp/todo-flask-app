6f8a12a6c0b24820283b20e86254b4cd3727633a
Daniel Floyd <gleek.dan@gmail.com>
2020-12-21 11:22:24 -0700

    add test_actions plan, add templates with test for title

---
+++ tests/test_actions.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_get_all_todos():
+    pytest.xfail()
+
+
+def test_add_todo():
+    pytest.xfail()
+
+
+def test_mark_complete():
+    pytest.xfail()
+
+
+def test_delete_todo():
+    pytest.xfail()
+
+
+def test_delete_todos_marked_complete():
+    pytest.xfail()
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,11 +1,4 @@
-import requests
 
-
-def test_hello_world_with_pyleniumio(py):
+def test_homepage_title(py):
     py.visit('http://localhost:5000')
-    assert py.contains('Hello World')
-
-
-def test_hello_world_with_requests():
-    response = requests.get('http://localhost:5000')
-    assert response.ok
+    assert py.title() == 'Todo App'
