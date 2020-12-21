008e54b46f978ae2ad2d29d84f190892e993deac
Daniel Floyd <gleek.dan@gmail.com>
2020-12-21 11:45:32 -0700

    add text_field and submit button

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,4 +1,15 @@
 
+BASE_URL = "http://localhost:5000"
+
+
 def test_homepage_title(py):
-    py.visit('http://localhost:5000')
+    py.visit(BASE_URL)
     assert py.title() == 'Todo App'
+
+
+def test_homepage_has_text_field_and_add_button(py):
+    py.visit(BASE_URL)
+    text_field = py.get('input')
+    add_button = py.get('button')
+    assert text_field.is_displayed()
+    assert add_button.is_displayed()
